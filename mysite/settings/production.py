ab60d1702322af7f676126957f66495f5ff6600b
Mwapsam <mwapsam@gmail.com>
2024-05-31 09:33:26 +0200

    Updates

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -2,5 +2,22 @@ from .base import *
 
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://rosegold-ent.com']
+CSRF_TRUSTED_ORIGINS = ["https://rosegold-ent.com"]
 
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = "DENY"
+
+ALLOWED_HOSTS = ["rosegold-ent.com", "www.rosegold-ent.com"]
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
