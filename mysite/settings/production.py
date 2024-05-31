aa0ff950b7af4fbbb52afed0e694cf1ad29faabd
Mwapsam <mwapsam@gmail.com>
2024-05-31 10:02:19 +0200

    Updates

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -23,6 +23,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
